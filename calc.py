f59b9253196d36383cadb95951a9d31a29e160e1
Liza Bialik <liza.bialik@gmail.com>
2019-11-19 14:56:12 -0500

    json reader working

--- calc.py
+++ calc.py
@@ -9,8 +9,8 @@ class Loan():
         self.interest_rate = float(interest_rate)
         self.subsidized = bool(sub)
         self.months_till_grad = self.parse_date(grad_date)
-    def parse_date(date_string):
-        # input = 05 2019 for May 2019
+    def parse_date(self, date_string):
+        # input = '05 2019' for May 2019
         month, year = date_string.split()
         today = datetime.date.today()
         grad = datetime.date(int(year), int(month), 30)
@@ -23,16 +23,16 @@ class Payment_Plan():
         self.loans = []
     def read_json(self, data):
         json_object = json.loads(data)
-        self.monthly_payment = float(json_object['monthly payment'])
-        self.number_of_months = float(json_object['num months'])
+        self.monthly_payment = float(json_object["monthly payment"])
+        self.number_of_months = float(json_object["num months"])
         loan_number = 0
-        for loan in json_object['loans']:
-            self.loans.append(Loan(loan_number, loan['amount'], loan['interest'], loan['subsidized'], loan['grad date']))
+        for loan in json_object["loans"]:
+            self.loans.append(Loan(loan_number, loan["amount"], loan["interest"], loan["subsidized"], loan["grad date"]))
             loan_number += 1
         print(self.loans)
     def consolidate(self):
         self.principal = sum([loan.amount for loan in self.loans])
-        self.avg_interest = sum([loan.interest for loan in self.loans]/len(loans))
+        self.avg_interest = sum([loan.interest_rate for loan in self.loans])/len(self.loans)
         print(self.principal)
         print(self.avg_interest)
     def months_to_pay(self):
@@ -43,9 +43,9 @@ class Payment_Plan():
         pass
     def make_json(self):
         data = {}
-        data['principal'] = self.principal
-        data['avg interest'] = self.avg_interest
-        data['graphs']['graph1'] = self.graph1_data()
+        data["principal"] = self.principal
+        data["avg interest"] = self.avg_interest
+        data["graphs"]["graph1"] = self.graph1_data()
         return json.dumps(data)
 
 PP = Payment_Plan()
@@ -53,4 +53,4 @@ PP.read_json(sys.argv[1])
 PP.consolidate()
 # PP.make_json()
 
-# {"monthly payment": 200, "num months": 0, "loans": [{"amount": 5000, "interest": 5, "grad date": 2016}, {"amount": 6000, "interest": 4.5, "grad date": 2016}]}
+# '{"monthly payment": 200, "num months": 0, "loans": [{"amount": 5000, "interest": 5, "subsidized": true, "grad date": "5 2016"}, {"amount": 6000, "interest": 4.5, "subsidized": false, "grad date": "12 2016"}]}'
