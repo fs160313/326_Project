c129a7123adcb097ec1a62cf1c86567154d5ead6
Sam Fuschetti <fs160313@gmail.com>
2019-12-05 13:33:33 -0500

    seperates posts

---
+++ server/config/routes.js
@@ -0,0 +1,23 @@
+// Require handlers we need for the user database as well as the loan database
+var userHandler = require('../databases/user_data/userHandler.js');
+var loanHandler = require('../databases/loan_data/loanHandler.js');
+
+// Function
+module.exports = function(app){
+
+  /*
+    Define routes here
+    Example route: app.post('api/deleteloan', userHandler.deleteLoan);
+  */
+
+  //routes for loan endpoints
+  app.post('/loan', loanHandler.addLoans)
+  app.post('/loan/mod', loanHandler.modifyLoan)
+  app.delete('/loan', loanHandler.deleteLoan) 
+
+  //routes for user endpoints
+  app.post('/user', userHandler.createUser)
+  app.put('/user/mod', userHandler.modifyUser)
+  app.delete('/user', userHandler.deleteUser)
+
+};
