8038b37fd54828edc439ec1ac102309545c09f85
Sam Fuschetti <fs160313@gmail.com>
2019-12-05 10:48:47 -0500

    adds initial route definitions

--- server/config/routes.js
+++ server/config/routes.js
@@ -9,6 +9,15 @@ module.exports = function(app){
     Define routes here
     Example route: app.post('api/deleteloan', userHandler.deleteLoan);
   */
-  
+
+  //routes for loan endpoints
+  app.post('/loan', loanHandler.addLoans)
+  app.put('loan', loanHandler.modifyLoan)
+  app.delete('/loan', loanHandler.deleteLoan) 
+
+  //routes for user endpoints
+  app.post('/user', userHandler.createUser)
+  app.put('/user', userHandler.modifyUser)
+  app.delete('/user', userHandler.deleteUser)
 
 };
