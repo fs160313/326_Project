5e1c6ee8ed6cb7440dc7effa6d6a09c262ec41f4
Sam Fuschetti <fs160313@gmail.com>
2019-12-06 21:21:57 -0500

    changes

--- public/js/bootstrap_custom.js
+++ public/js/bootstrap_custom.js
@@ -1,5 +1,5 @@
 var title = document.getElementById('title');
-var list = document.getElementById('list');
+var list = document.getElementById('loanlist');
 var li = list.getElementsByTagName('li');
 var addBtn = document.getElementById('add-Btn');
 var loanBtn = document.getElementById('part_4');
@@ -21,8 +21,21 @@ loanBtn.addEventListener('click', function() {
   var amount = document.getElementById("part_1").value;
   var interest = document.getElementById("part_2").value;
   var sub = document.getElementById("part_3").value;
-  list.innerHTML += '<li class="list-group-item">' + "$" + amount + ",  %" + interest + ",  " + sub + '&nbsp;' + ' <button> - </button>' + '</li>';
-  $('#modalLoanForm').modal('hide')
+  //list.innerHTML += '<li class="list-group-item">' + "$" + amount + ",  %" + interest + ",  " + sub + '&nbsp;' + ' <button> - </button>' + '</li>';
+  $("#loanlist").append(`<li>
+                      <ul>
+                        <li class='amount'>
+                          ${amount}
+                        </li>
+                        <li class='interest'>
+                          ${interest}
+                        </li>
+                        <li class='subsidized'>
+                          ${sub}
+                        </li>
+                      </ul>
+                  </li>`);
+  $('#modalLoanForm').modal('hide');
   $('#modalLoanForm').trigger("reset");
 });
 
@@ -30,3 +43,20 @@ $("ul").on("click", "button", function(e) {
   e.preventDefault();
   $(this).parent().remove();
 });
+$("#calculate")[0].addEventListener('click', function() {
+  let grad_date = $("#months").children('option:selected').val() + " " + $("#year").val()
+  let monthly_payment = $("#monthlypayment").val()
+  let loans = [];
+  let list = $("#loanlist");
+  
+    
+    // console.log(loan);
+    // loanamt = $(this).getElementById('amount');
+    // interestamt = $(this).getElementById('interest');
+    // sub = $(this).getElementById('subsidized');
+    // loan['']
+    // // let loan = [($(this).val())[0], ($(this).val())[1], ($(this).val())[2]]
+    // // loans.push(loan);
+  })
+  //console.log(loans);
+})
